2f4160251d7194594490e784fae3b864adb3c905
Sebastian Betancur <sebbet26@bergen.org>
2024-03-12 09:04:09 -0400

    d

--- error.py
+++ error.py
@@ -1,1 +1,14 @@
+import csv, json, re
 
+with open("logs.json") as jsonFile:
+    data = json.load(jsonFile)
+    pattern = r"ERROR"
+    li = []
+    for entry in data:
+        match = re.match(pattern, entry["level"], re.IGNORECASE)
+        li.append(match)
+    with open("logs.csv", "w+", newline='') as csvFile:
+        for item in li:
+            if item != None:
+                writer = csv.DictWriter()
+                writer.writeheader("timestamp", "level", "message")
